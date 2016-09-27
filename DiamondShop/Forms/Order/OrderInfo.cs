8634d1bd7370a654d2ed6726af21f76e06976099
weng007 <awoot.r@gmail.com>
2016-09-27 11:53:20 +0700

    w

--- DiamondShop/Forms/Order/OrderInfo.cs
+++ DiamondShop/Forms/Order/OrderInfo.cs
@@ -363,7 +363,7 @@ namespace DiamondShop
             }
             else
             {
-                RequirePassword frm = new RequirePassword("2",ApplicationInfo.Shop);
+                RequirePassword frm = new RequirePassword(ApplicationInfo.Shop);
                 frm.ShowDialog();
                 isAuthorize = frm.isAuthorize;
                 frm.Close();
